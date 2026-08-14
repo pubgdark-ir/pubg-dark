fffcc299e678dd86b25df578b10a9e99ab90c5c9
pubgdark-ir <nikpoor800@gmail.com>
2026-08-14 21:28:56 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+console.log("PUBG DARK Ready");
+
+document.querySelectorAll("button").forEach(function(btn){
+btn.addEventListener("click", function(){
+alert("برای خرید وارد تلگرام PUBG DARK شوید");
+});
+});
+
+});
